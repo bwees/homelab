56ce926dafcf197003d0452b59032cf66783c098
bwees <brandonwees@gmail.com>
2025-10-06 19:31:08 -0500

    chore: delete unused configs

--- justfile
+++ justfile
@@ -14,15 +14,3 @@ deploy HOST="all:!homelab-router":
     ansible-playbook playbooks/deploy.yml --limit {{ HOST }}
     
     @just _cleanup_secrets
-
-[working-directory: 'ansible']
-config HOST="all:!homelab-router":
-    ansible-playbook playbooks/config.yml --limit {{ HOST }}
-    
-[working-directory: 'ansible']
-tailscale-update HOST:
-    ansible-playbook playbooks/tailscale-update.yml --limit {{ HOST }}
-
-[working-directory: 'ansible']
-zfs-autosnap HOST:
-    ansible-playbook playbooks/zfs-autosnap.yml --limit {{ HOST }}
--- scripts/homelab-bwees-snapshot.sh
+++ scripts/homelab-bwees-snapshot.sh
@@ -2,7 +2,23 @@
 
 # This script creates a new ZFS snapshot and keeps only the 4 most recent snapshots.
 # It is intended to be run as a cron job, every 12 hours.
-# Install it with `just zfs-autosnap <HOST>`.
+# Install it with 
+# - name: Transfer docker-compose files to host
+#   copy:
+#     src: '../../scripts/homelab-bwees-snapshot.sh'
+#     dest: '/home/zfsrepl/zfs-snapshot.sh'
+#     mode: '0755'
+#     force: true
+
+# - name: Create cron job for ZFS Auto-snapshot, run every 12 hours
+#   cron:
+#     name: 'ZFS Auto-snapshot'
+#     minute: '0'
+#     hour: '*/12'
+#     job: '/home/zfsrepl/zfs-snapshot.sh'
+#     user: 'zfsrepl'
+#     state: present
+
 
 # Define dataset
 DATASET="storage"  # Change this to your ZFS dataset
