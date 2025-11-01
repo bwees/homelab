fd484697a8a29f440d2ca58c46f53aecdf595ff3
bwees <brandonwees@gmail.com>
2025-11-01 18:19:46 -0500

    feat: setup nixos anywhere

---
+++ nixos/anywhere/README.md
@@ -0,0 +1,3 @@
+```
+nixos-anywhere --flake .#generic --generate-hardware-config nixos-generate-config ./hardware-configuration.nix --build-on remote bwees@10.0.10.25
+```
---
+++ nixos/anywhere/disk-config.nix
@@ -0,0 +1,38 @@
+{ lib, ... }:
+{
+  disko.devices = {
+    disk.sda = {
+      device = "/dev/sda";
+      type = "disk";
+      content = {
+        type = "gpt";
+        partitions = {
+          boot = {
+            name = "boot";
+            size = "1G";
+            type = "EF00";
+            content = {
+              type = "filesystem";
+              format = "vfat";
+              mountpoint = "/boot";
+            };
+          };
+          swap = {
+            size = "4G";
+            content = {
+              type = "swap";
+            };
+          };
+          root = {
+            size = "100%";
+            content = {
+              type = "filesystem";
+              format = "ext4";
+              mountpoint = "/";
+            };
+          };
+        };
+      };
+    };
+  };
+}
