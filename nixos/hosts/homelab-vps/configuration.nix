2f8bef2dc3682f1d9c438035455a75f07918d23d
bwees <brandonwees@gmail.com>
2025-12-16 23:48:12 -0600

    feat: add homelab-vps

---
+++ nixos/hosts/homelab-vps/configuration.nix
@@ -0,0 +1,29 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+
+{
+  imports = [
+    ./hardware-configuration.nix
+    ./disk-config.nix
+    ../../lib/base_packages.nix
+    ../../lib/bwees.nix
+    ../../lib/docker.nix
+    ../../lib/garbage_collect.nix
+    ../../lib/root_ca.nix
+    ../../lib/tailscale.nix
+  ];
+
+  system.stateVersion = "25.05"; # Did you read the comment?
+
+  # Networking/Clock
+  networking.hostName = "homelab-vps";
+  services.tailscale.ip = "100.105.77.106";
+  time.timeZone = "America/Chicago";
+
+  services.openssh.enable = true;
+  services.openssh.settings.PasswordAuthentication = false;
+}
