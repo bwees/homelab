5a147343af00739cc283d68c23625464e80e963e
bwees <brandonwees@gmail.com>
2026-06-12 17:55:49 -0500

    fix: UDP gro forwarding

--- nixos/lib/tailscale.nix
+++ nixos/lib/tailscale.nix
@@ -1,4 +1,5 @@
 {
+  pkgs,
   ...
 }:
 
@@ -6,5 +7,28 @@
   config = {
     services.tailscale.enable = true;
     services.tailscale.useRoutingFeatures = "server";
+
+    # fixes https://tailscale.com/s/ethtool-config-udp-gro
+    systemd.services.enable-udp-gro-forwarding = {
+      description = "Enable UDP GRO forwarding for Tailscale";
+      wantedBy = [ "multi-user.target" ];
+      after = [ "network-online.target" ];
+      wants = [ "network-online.target" ];
+
+      serviceConfig.Type = "oneshot";
+
+      script = ''
+        iface=$(${pkgs.iproute2}/bin/ip route show default | ${pkgs.gawk}/bin/awk '{print $5}' | head -n1)
+
+        if [ -n "$iface" ]; then
+          echo "Enabling UDP GRO forwarding on $iface"
+          ${pkgs.ethtool}/bin/ethtool -K "$iface" rx-udp-gro-forwarding on
+          ${pkgs.ethtool}/bin/ethtool -K "$iface" rx-gro-list off
+        else
+          echo "No default interface found"
+          exit 1
+        fi
+      '';
+    };
   };
 }
