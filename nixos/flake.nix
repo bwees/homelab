90f73314cbb97fd465cda06a7fbae68c188ec218
bwees <brandonwees@gmail.com>
2026-06-17 11:42:19 -0500

    chore(nix): update stable packages to 26.05

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-unstable";
-    nixpkgs-stable.url = "nixpkgs/nixos-25.11";
+    nixpkgs-stable.url = "nixpkgs/nixos-26.05";
     disko.url = "github:nix-community/disko";
   };
 
