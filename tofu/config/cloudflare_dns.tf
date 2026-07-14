898153512e035dbbeb541ad42a4e614c7342a369
bwees <brandonwees@gmail.com>
2026-07-14 10:40:20 -0500

    fix: wildcard tunnel naming

--- tofu/config/cloudflare_dns.tf
+++ tofu/config/cloudflare_dns.tf
@@ -2,7 +2,7 @@ resource "cloudflare_dns_record" "bwees_io_wildcard" {
   zone_id = local.bwees_io_zone
   name    = "*"
   type    = "CNAME"
-  content = "${cloudflare_dns_record.tunnels["tau-ceti"].name}.bwees.io"
+  content = cloudflare_dns_record.tunnels["tau-ceti"].name
   comment = "Managed by Tofu."
   proxied = true
   ttl     = 1
