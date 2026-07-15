ec6c685fd776086c2dc7afa806dfbf044fe6387b
bwees <brandonwees@gmail.com>
2026-07-14 20:43:07 -0500

    feat: external dns api key

---
+++ tofu/config/api_keys.tf
@@ -0,0 +1,38 @@
+
+resource "cloudflare_api_token" "external_dns" {
+  name = "k3s External DNS"
+  policies = [
+    {
+      effect = "allow"
+      resources = jsonencode({
+        "com.cloudflare.api.account.zone.72e8e948ac04faef676a0a877bab6f9d" = "*" # zone: bwees.io
+      })
+      permission_groups = [
+        { id = "4755a26eedb94da69e1066d98aa820be" }, # DNS Write
+      ]
+    }
+  ]
+  status    = "active"
+  condition = {}
+}
+
+resource "onepassword_item" "cf_external_dns" {
+  vault    = data.onepassword_vault.homelab_deployment.uuid
+  title    = "cf-external-dns"
+  category = "login"
+
+  section_map = {
+    "credentials" = {
+      field_map = {
+        "zone" = {
+          type  = "STRING"
+          value = data.cloudflare_zone.bwees_io.zone_id
+        }
+        "token" = {
+          type  = "CONCEALED"
+          value = cloudflare_api_token.external_dns.value
+        }
+      }
+    }
+  }
+}
--- tofu/config/locals.tf
+++ tofu/config/locals.tf
@@ -1,7 +1,10 @@
 
-locals {
-  cloudflare_account_id = "5e2ba2ec4aedeea294c2bf45f28c6414"
-  bwees_io_zone         = "72e8e948ac04faef676a0a877bab6f9d"
+data "cloudflare_account" "main" {
+  account_id = "5e2ba2ec4aedeea294c2bf45f28c6414"
+}
+
+data "cloudflare_zone" "bwees_io" {
+  zone_id = "72e8e948ac04faef676a0a877bab6f9d"
 }
 
 data "onepassword_vault" "homelab_deployment" {
