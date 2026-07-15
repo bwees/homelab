345e09d69611b4ff1ac90ddb5f76b0760419d7aa
bwees <brandonwees@gmail.com>
2026-07-14 23:55:37 -0500

    chore: cf api token resource reference zone id

--- tofu/config/api_keys.tf
+++ tofu/config/api_keys.tf
@@ -5,7 +5,7 @@ resource "cloudflare_api_token" "external_dns" {
     {
       effect = "allow"
       resources = jsonencode({
-        "com.cloudflare.api.account.zone.72e8e948ac04faef676a0a877bab6f9d" = "*" # zone: bwees.io
+        "com.cloudflare.api.account.zone.${data.cloudflare_zone.bwees_io.zone_id}" = "*"
       })
       permission_groups = [
         { id = "4755a26eedb94da69e1066d98aa820be" }, # DNS Write
