b98cd53600aec2a93a2c14a72a682528b4aa4800
bwees <brandonwees@gmail.com>
2026-08-06 23:50:36 -0500

    feat(dns): point the bwees.io wildcard at tau-ceti and drop the cloudflare tunnels

--- tofu/config/api_keys.tf
+++ tofu/config/api_keys.tf
@@ -1,13 +1,15 @@
-
-resource "cloudflare_account_token" "external_dns" {
+// cert-manager DNS-01 for the *.bwees.io wildcard. Zone Read is required
+// alongside DNS Write so it can look the zone up before writing the challenge.
+resource "cloudflare_account_token" "cert_manager" {
   account_id = data.cloudflare_account.main.id
-  name       = "k3s External DNS"
+  name       = "k3s cert-manager DNS01"
 
   policies = [
     {
       effect = "allow"
       permission_groups = [
         { id = "4755a26eedb94da69e1066d98aa820be" }, # DNS Write
+        { id = "c8fed203ed3043cba015a93ad1616f1f" }, # Zone Read
       ]
       resources = jsonencode({
         "com.cloudflare.api.account.zone.${data.cloudflare_zone.bwees_io.zone_id}" = "*"
@@ -16,27 +18,22 @@ resource "cloudflare_account_token" "external_dns" {
   ]
 }
 
-resource "onepassword_item" "cf_external_dns" {
+resource "onepassword_item" "cf_cert_manager" {
   vault    = data.onepassword_vault.homelab_deployment.uuid
-  title    = "cf-external-dns"
+  title    = "cf-cert-manager"
   category = "password"
 
   section_map = {
     "credentials" = {
       field_map = {
-        "zone" = {
-          type  = "STRING"
-          value = data.cloudflare_zone.bwees_io.zone_id
-        }
         "token" = {
           type  = "CONCEALED"
-          value = cloudflare_account_token.external_dns.value
+          value = cloudflare_account_token.cert_manager.value
         }
       }
     }
   }
 }
-
 
 resource "cloudflare_account_token" "pages_deployment" {
   account_id = data.cloudflare_account.main.id
--- tofu/config/dns.tf
+++ tofu/config/dns.tf
@@ -1,9 +1,9 @@
 resource "cloudflare_dns_record" "bwees_io_wildcard" {
   zone_id = data.cloudflare_zone.bwees_io.zone_id
   name    = "*"
-  type    = "CNAME"
-  content = cloudflare_dns_record.tunnels["tau-ceti"].name
-  comment = "Managed by Tofu."
-  proxied = true
-  ttl     = 1
+  type    = "A"
+  content = "45.137.192.163"
+  comment = "Public ingress on tau-ceti. Managed by Tofu."
+  proxied = false
+  ttl     = 300
 }
