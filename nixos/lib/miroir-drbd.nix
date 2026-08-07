97635df4708b4c83ddcbd2afda24d5ccda8ca9e1
bwees <brandonwees@gmail.com>
2026-08-07 16:41:18 -0500

    chore: nixos update

--- nixos/lib/miroir-drbd.nix
+++ nixos/lib/miroir-drbd.nix
@@ -1,11 +1,16 @@
 { config, pkgs, ... }:
 
 let
-  drbd9 = config.boot.kernelPackages.drbd.overrideAttrs (_: {
+  drbd9 = config.boot.kernelPackages.drbd.overrideAttrs (prev: {
     version = "9.3.3";
     src = pkgs.fetchurl {
       url = "https://pkg.linbit.com/downloads/drbd/9/drbd-9.3.3.tar.gz";
       hash = "sha256-p7+wFgcMMd8cc4VpyozF5fwzfdRJFH979i50bYfTjyE=";
+    };
+    
+    # upstream marks 9.3.2 broken on kernel 6.18; 9.3.3 is the release that fixes it
+    meta = prev.meta // {
+      broken = false;
     };
   });
 in
